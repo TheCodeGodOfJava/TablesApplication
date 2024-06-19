0f985b794cb50d50c9dabd77745fc7a505a8201c
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-06-19 22:48:26 +0300

    Make marquee scroll

--- src/app/directives/mat-label-scroll/mat-label-scroll.directive.ts
+++ src/app/directives/mat-label-scroll/mat-label-scroll.directive.ts
@@ -1,92 +1,66 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Directive({
   selector: '[appMatLabelScroll]',
   standalone: true,
 })
-export class MatLabelScrollDirective {
-  styleElement: HTMLStyleElement = this.renderer.createElement('style');
-  mainPositionClassName: string = 'label-main-position';
-  topPositionClassName: string = 'label-top-position';
-  matLabelElement: HTMLElement = {} as HTMLElement;
-  labelElement: HTMLElement = {} as HTMLElement;
-  indent: number = 16;
+export class MatLabelScrollDirective implements AfterViewInit, OnDestroy {
+  private styleElement: HTMLStyleElement;
+  private labelElement!: HTMLElement;
+  private mutationObserver!: MutationObserver;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  @HostListener('mouseleave', ['$event'])
-  @HostListener('input', ['$event'])
-  @HostListener('click', ['$event'])
-  handleEvent() {
-    this.clearClasses();
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.styleElement = this.renderer.createElement('style');
+    this.initAnimationStyles();
   }
 
-  @HostListener('mouseenter') onMouseEnter() {
+  ngAfterViewInit() {
     this.labelElement = this.el.nativeElement.querySelector('label');
-    this.matLabelElement = this.el.nativeElement.querySelector('mat-label');
+    if (this.labelElement) {
+      this.startMarquee();
 
-    if (!this.labelElement) {
-      return;
+      this.mutationObserver = new MutationObserver(() => {
+        this.handleMutation();
+      });
+      this.mutationObserver.observe(this.labelElement, {
+        childList: true,
+        characterData: true,
+        subtree: true,
+      });
     }
+  }
 
-    let isLabelTopPosition = this.labelElement.classList.contains(
-      'mdc-floating-label--float-above'
-    );
-    let labelWrapperWidth = this.el.nativeElement.getBoundingClientRect().width;
-    let isLabelWider = this.getCorrectLabelWidth() > labelWrapperWidth;
-
-    if (!isLabelWider) {
-      return;
+  ngOnDestroy() {
+    if (this.mutationObserver) {
+      this.mutationObserver.disconnect();
     }
-
-    let widthDifference = this.getCorrectLabelWidth() - labelWrapperWidth;
-
-    isLabelTopPosition
-      ? this.generateLabelTopPositionClass(widthDifference)
-      : this.generateLabelMainPositionClass(widthDifference);
   }
 
-  generateClass(className: string, styles: string) {
-    this.styleElement.textContent = `.${className} { ${styles} }`;
-    this.renderer.appendChild(this.el.nativeElement, this.styleElement);
-    this.renderer.addClass(this.labelElement, className);
+  initAnimationStyles() {
+    const styles = `
+      @keyframes marquee {
+        0% { transform: translateX(100%); }
+        100% { transform: translateX(-100%); }
+      }
+
+      .marquee {
+        display: inline-block;
+        white-space: nowrap;
+        overflow: hidden;
+        animation: marquee 10s linear infinite;
+      }
+    `;
+    this.styleElement.textContent = styles;
+    this.renderer.appendChild(document.head, this.styleElement);
   }
 
-  generateLabelMainPositionClass(transition: number) {
-    let styles = `animation: marquee 10s linear infinite !important; transition-delay: 0.1s !important; transform: translate(-${
-      transition + this.indent * 3
-    }px, -50%) !important;`;
-    let selectElNecessaryStyles =
-      'text-overflow: clip !important; overflow: visible !important; max-width:none !important; padding-right: 10px !important';
-    styles += selectElNecessaryStyles;
-
-    this.generateClass(this.mainPositionClassName, styles);
+  startMarquee() {
+    this.renderer.addClass(this.labelElement, 'marquee');
   }
 
-  generateLabelTopPositionClass(transition: number) {
-    let styles = `transition: transform 2s ease-in-out !important; transition-delay: 0.1s !important; transform: translate(-${
-      transition / 0.75 + this.indent * 2
-    }px,-106%) scale(0.75) !important;`;
-    let selectElNecessaryStyles =
-      'text-overflow: clip !important; overflow: visible !important; max-width:none !important;';
-    styles += selectElNecessaryStyles;
-
-    this.generateClass(this.topPositionClassName, styles);
-  }
-
-  getCorrectLabelWidth() {
-    let matLabelWidth = this.matLabelElement.getBoundingClientRect().width;
-    let labelWidth = this.labelElement.getBoundingClientRect().width;
-    return matLabelWidth > labelWidth ? matLabelWidth : labelWidth;
-  }
-
-  clearClasses() {
-    if (!this.labelElement) {
-      return;
-    }
-
-    this.renderer.removeClass(this.labelElement, this.topPositionClassName);
-    this.renderer.removeClass(this.labelElement, this.mainPositionClassName);
-    this.renderer.removeChild(this.labelElement, this.styleElement);
+  handleMutation() {
+    this.renderer.removeClass(this.labelElement, 'marquee');
+    void this.labelElement.offsetWidth; // Trigger reflow
+    this.renderer.addClass(this.labelElement, 'marquee');
   }
 }
