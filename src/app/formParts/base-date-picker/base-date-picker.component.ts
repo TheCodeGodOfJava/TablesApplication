0e172634a0f7722658865de5d5e2b68da22aab5c
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-06-21 11:55:06 +0300

    Add base date picker component

---
+++ src/app/formParts/base-date-picker/base-date-picker.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatLabelScrollDirective } from '../../directives/mat-label-scroll/mat-label-scroll.directive';
+import { AbstractFormComponent } from '../abstract/abstractFormComponent';
+
+@Component({
+  selector: 'base-date-picker',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatLabelScrollDirective,
+  ],
+  providers: [DatePipe],
+  templateUrl: './base-date-picker.component.html',
+  styleUrl: './base-date-picker.component.scss',
+})
+export class BaseDatePickerComponent extends AbstractFormComponent {}
