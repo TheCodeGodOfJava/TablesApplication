0880d6e90b2d58a3112166dd4e0b63b83cbfc482
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-06-23 19:50:20 +0300

    Add tests

--- src/app/formParts/base-select/base-select.component.spec.ts
+++ src/app/formParts/base-select/base-select.component.spec.ts
@@ -1,8 +1,8 @@
-import { CommonModule } from '@angular/common';
 import {
   ComponentFixture,
   TestBed,
   fakeAsync,
+  flush,
   tick,
 } from '@angular/core/testing';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -11,7 +11,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { Observable, of } from 'rxjs';
-import { SELECT_SEARCH_PREFIX } from '../../constants';
 import { FilterService } from '../../services/filter/filter.service';
 import { BaseSelectComponent } from './base-select.component';
 
@@ -19,7 +18,8 @@ class MockFilterService {
   getDataForFilter(
     controllerPath: string,
     field: string,
-    term: string
+    term: string,
+    dep: string = ''
   ): Observable<string[]> {
     const mockData: string[] = ['example1', 'example2', 'example3'];
     return of(mockData);
@@ -36,30 +36,66 @@ describe('BaseSelectComponent', () => {
 
     await TestBed.configureTestingModule({
       imports: [
-        CommonModule,
         MatFormFieldModule,
         MatSelectModule,
         ReactiveFormsModule,
         NgxMatSelectSearchModule,
-        BrowserAnimationsModule, // Import BrowserAnimationsModule
-        BaseSelectComponent, // Import the standalone component here
+        BrowserAnimationsModule,
+        BaseSelectComponent,
       ],
       providers: [{ provide: FilterService, useValue: mockFilterService }],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(BaseSelectComponent);
-    component = fixture.componentInstance;
   });
 
-  it('should create the component', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BaseSelectComponent);
+    component = fixture.componentInstance;
     component.controllerPath = 'testPath';
     component.alias = 'testAlias';
     component.placeholder = 'testPlaceholder';
+    component.dependentAliases = ['dependentAlias'];
     component.formGroup = new FormGroup({
-      testAliasSelectSearch: new FormControl<string | null>(null),
-      testAlias: new FormControl<string[]>([]),
+      testAliasSelectSearch: new FormControl<string>(''),
+      testAlias: new FormControl<string>('option1'),
+      dependentAlias: new FormControl<string>('depOption'),
     });
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+
+  it('should create the component', () => {
     expect(component).toBeTruthy();
-  }); 
+  });
+
+  it('should load options correctly', fakeAsync(() => {
+    component.ngAfterViewInit();
+    component['loadOptionsForSelect']('testAlias', 'searchTerm');
+    tick(700); // debounce time
+
+    const optionsData = component['getOptionsData']('testAlias');
+    optionsData.options.subscribe((options) => {
+      expect(options).toEqual(['example1', 'example2', 'example3']);
+    });
+  }));
+
+  it('should update dependent aliases on value changes', fakeAsync(() => {
+    // Simulate ngAfterViewInit lifecycle hook
+    component.ngAfterViewInit();
+
+    // Trigger value change
+    component.formGroup.get('testAlias')?.setValue(['newOption']);
+    tick(700); // Adjust timing to match debounceTime in your component
+
+    // Ensure all pending asynchronous tasks are completed
+    flush();
+
+    // Assert the dependent options data
+    const dependentOptionsData = component['getOptionsData']('dependentAlias');
+    dependentOptionsData.options.subscribe((options) => {
+      expect(options).toEqual(['example1', 'example2', 'example3']);
+    });
+  }));
 });
