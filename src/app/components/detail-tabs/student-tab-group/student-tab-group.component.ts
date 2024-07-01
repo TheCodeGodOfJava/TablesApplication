55c4254296e659d4d466845e0b92809cfab2009e
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-07-01 20:33:29 +0300

    Add student tab group

--- src/app/components/detail-tabs/student-tab-group/student-tab-group.component.ts
+++ src/app/components/detail-tabs/student-tab-group/student-tab-group.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { StudentFromComponent } from '../../form/components/student-form/student-form.component';
 
 @Component({
   selector: 'student-tab-group',
   standalone: true,
   templateUrl: './student-tab-group.component.html',
   styleUrl: './student-tab-group.component.scss',
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, StudentFromComponent],
 })
-export class StudentTabGroupComponent {}
+export class StudentTabGroupComponent {
+  @Input()
+  detailId!: number;
+}
