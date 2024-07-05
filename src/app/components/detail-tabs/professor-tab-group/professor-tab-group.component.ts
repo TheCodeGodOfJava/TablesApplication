fb5a68af8d18bdf238d34215b319c5fc9284a5db
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-07-05 14:35:03 +0300

    Fix spelling

--- src/app/components/detail-tabs/professor-tab-group/professor-tab-group.component.ts
+++ src/app/components/detail-tabs/professor-tab-group/professor-tab-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ProfessorFromComponent } from '../../form/components/professor-form/professor-form.component';
 
 @Component({
   selector: 'professor-tab-group',
