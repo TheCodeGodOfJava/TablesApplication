a0eeb4827c6247226d5c7a8e85137657a65477af
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-08-13 15:27:42 +0300

    Move table actions out of controller

--- src/app/components/data-tables/tableActions.ts
+++ src/app/components/data-tables/tableActions.ts
@@ -2,13 +2,119 @@ import { FormArray, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Id } from '../../models/id';
 import { StateService } from '../../services/state/state.service';
-import { AbstractActions } from '../actions';
+import { AbstractActions } from '../abstractActions';
 import { GenericDataSource } from './components/abstract/genericDataSource';
 import { ACTIONS, AppAction } from './interfaces/appAction';
 import { AppEntity } from './interfaces/appEntity';
 
 export class TableActions<T extends Id> extends AbstractActions<T> {
-  allActions!: AppAction<T>[];
+  override allActions = [
+    {
+      type: ACTIONS.EDIT,
+      icon: 'create',
+      getAction: (model: T, index: number) => {
+        model.visible = true;
+        this.changeVisibilityForUnselectedRows(index);
+      },
+      getShowCondition: (model: T) => !model.visible,
+      description: 'Edit',
+    },
+    {
+      type: ACTIONS.SAVE,
+      icon: 'check',
+      getAction: (model: T, index: number) => {
+        const rowFormGroup = this.getRowFormGroup(index);
+        if (rowFormGroup.invalid) {
+          this.toastrService.error('Your form is invalid. Validate it first!');
+        } else {
+          const model = rowFormGroup.value;
+          const data = this.dataSource.modelSubject.getValue();
+          data[index] = model;
+          this.dataSource.modelSubject.next(data);
+          model.visible = false;
+          super.saveAction(model);
+        }
+      },
+      getShowCondition: (model: T) => !!model.visible,
+      description: 'Save',
+    },
+    {
+      type: ACTIONS.CANCEL,
+      icon: 'cancel',
+      getAction: (model: T, index: number) => (model.visible = false),
+      getShowCondition: (model: T) => !!model.visible,
+      description: 'Cancel',
+    },
+    {
+      type: ACTIONS.REMOVE,
+      icon: 'remove',
+      getAction: (model: T, index: number) => {
+        this.removeRow(index);
+        if (model.id) {
+          this.stateService.remove(this.controllerPath, [model.id]).subscribe({
+            next: () =>
+              this.toastrService.success('Row data successfully deleted!'),
+            error: (error) => {
+              console.error('error:', error);
+              this.toastrService.error(
+                'Error occured! See console log for details!'
+              );
+            },
+          });
+        }
+      },
+      getShowCondition: (model: T) => !model.visible,
+      description: 'Remove',
+    },
+    {
+      type: ACTIONS.UNBIND,
+      icon: 'playlist_remove',
+      getAction: (model: T, index: number) => {
+        this.removeRow(index);
+        if (model.id && this.masterId) {
+          this.stateService
+            .unbind(this.controllerPath, [model.id], this.masterId)
+            .subscribe({
+              next: () =>
+                this.toastrService.success('Row data successfully unbound!'),
+              error: (error) => {
+                console.error('error:', error);
+                this.toastrService.error(
+                  'Error occured! See console log for details!'
+                );
+              },
+            });
+        }
+      },
+      getShowCondition: (model: T) =>
+        !model.visible && !!this.masterId && !this.tableData.toggled,
+      description: 'Unbind',
+    },
+    {
+      type: ACTIONS.BIND,
+      icon: 'playlist_add',
+      getAction: (model: T, index: number) => {
+        this.removeRow(index);
+        if (model.id && this.masterId) {
+          this.stateService
+            .bind(this.controllerPath, [model.id], this.masterId)
+            .subscribe({
+              next: () =>
+                this.toastrService.success('Row data successfully bound!'),
+              error: (error) => {
+                console.error('error:', error);
+                this.toastrService.error(
+                  'Error occured! See console log for details!'
+                );
+              },
+            });
+        }
+      },
+      getShowCondition: (model: T) =>
+        !model.visible && !!this.masterId && this.tableData.toggled,
+      description: 'Bind',
+    },
+  ];
 
   constructor(
     protected tableData: { toggled: boolean },
@@ -20,117 +126,6 @@ export class TableActions<T extends Id> extends AbstractActions<T> {
     protected override toastrService: ToastrService
   ) {
     super(controllerPath, stateService, toastrService);
-    this.allActions = [
-      {
-        type: ACTIONS.EDIT,
-        icon: 'create',
-        getAction: (model: T, index: number) => {
-          model.visible = true;
-          this.changeVisibilityForUnselectedRows(index);
-        },
-        getShowCondition: (model: T) => !model.visible,
-        description: 'Edit',
-      },
-      {
-        type: ACTIONS.SAVE,
-        icon: 'check',
-        getAction: (model: T, index: number) => {
-          const rowFormGroup = this.getRowFormGroup(index);
-          if (rowFormGroup.invalid) {
-            this.toastrService.error(
-              'Your form is invalid. Validate it first!'
-            );
-          } else {
-            const model = rowFormGroup.value;
-            const data = this.dataSource.modelSubject.getValue();
-            data[index] = model;
-            this.dataSource.modelSubject.next(data);
-            model.visible = false;
-            super.saveAction(model);
-          }
-        },
-        getShowCondition: (model: T) => !!model.visible,
-        description: 'Save',
-      },
-      {
-        type: ACTIONS.CANCEL,
-        icon: 'cancel',
-        getAction: (model: T, index: number) => (model.visible = false),
-        getShowCondition: (model: T) => !!model.visible,
-        description: 'Cancel',
-      },
-      {
-        type: ACTIONS.REMOVE,
-        icon: 'remove',
-        getAction: (model: T, index: number) => {
-          this.removeRow(index);
-          if (model.id) {
-            this.stateService
-              .remove(this.controllerPath, [model.id])
-              .subscribe({
-                next: () =>
-                  this.toastrService.success('Row data successfully deleted!'),
-                error: (error) => {
-                  console.error('error:', error);
-                  this.toastrService.error(
-                    'Error occured! See console log for details!'
-                  );
-                },
-              });
-          }
-        },
-        getShowCondition: (model: T) => !model.visible,
-        description: 'Remove',
-      },
-      {
-        type: ACTIONS.UNBIND,
-        icon: 'playlist_remove',
-        getAction: (model: T, index: number) => {
-          this.removeRow(index);
-          if (model.id && this.masterId) {
-            this.stateService
-              .unbind(this.controllerPath, [model.id], this.masterId)
-              .subscribe({
-                next: () =>
-                  this.toastrService.success('Row data successfully unbound!'),
-                error: (error) => {
-                  console.error('error:', error);
-                  this.toastrService.error(
-                    'Error occured! See console log for details!'
-                  );
-                },
-              });
-          }
-        },
-        getShowCondition: (model: T) =>
-          !model.visible && !!this.masterId && !this.tableData.toggled,
-        description: 'Unbind',
-      },
-      {
-        type: ACTIONS.BIND,
-        icon: 'playlist_add',
-        getAction: (model: T, index: number) => {
-          this.removeRow(index);
-          if (model.id && this.masterId) {
-            this.stateService
-              .bind(this.controllerPath, [model.id], this.masterId)
-              .subscribe({
-                next: () =>
-                  this.toastrService.success('Row data successfully bound!'),
-                error: (error) => {
-                  console.error('error:', error);
-                  this.toastrService.error(
-                    'Error occured! See console log for details!'
-                  );
-                },
-              });
-          }
-        },
-        getShowCondition: (model: T) =>
-          !model.visible && !!this.masterId && this.tableData.toggled,
-        description: 'Bind',
-      },
-    ];
   }
 
   private removeRow(index: number): void {
