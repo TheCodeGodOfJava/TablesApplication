8db5dc501ef5a282d60f0fe35429f46b25874f0c
Serhiy Ivanov <s.ivanov@flex-solution.com>
2024-08-14 13:28:37 +0300

    Refactor form context menu actions

--- src/app/components/form/formContextMenuActions.ts
+++ src/app/components/form/formContextMenuActions.ts
@@ -1,22 +1,66 @@
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Id } from '../../models/id';
-import { ACTIONS } from '../data-tables/interfaces/appAction';
+import { ACTIONS, AppAction } from '../data-tables/interfaces/appAction';
 import { AppEntity } from '../data-tables/interfaces/appEntity';
+import { CONTROL_TYPE } from '../data-tables/interfaces/inputTypes';
 import { ProtoActions } from '../protoActions';
+import { TileOperations } from './components/abstract/tile-operations';
+import { ToastrService } from 'ngx-toastr';
 
 export class FormContextMenuActions<T extends Id> extends ProtoActions<
   AppEntity<T>
 > {
-  override allActions = [
+  currentFormElementForContextMenu!: AppEntity<T>;
+
+  override allActions: AppAction<AppEntity<T>>[] = [
     {
-      type: ACTIONS.DISABLE,
-      icon: 'create',
-      getAction: (model: AppEntity<T>, index: number) => {},
+      type: ACTIONS.STATE,
+      icon: 'published_with_changes',
       getShowCondition: (model: AppEntity<T>) => true,
-      description: 'Edit',
+      description: 'Enable/Disable',
+      appEntity: {
+        alias: 'changeStateAction',
+        placeholder: '',
+        mainControl: {
+          type: CONTROL_TYPE.BOOLEAN,
+          getControl: () => new FormControl<boolean | null>(null),
+        },
+        action: (alias: string, fromGroup: FormGroup) => {
+          const value: boolean = fromGroup.get(alias)?.value;
+          this.currentFormElementForContextMenu.disabled = !value;
+          const mainGroupFormControl = this.mainFormGroup.get(
+            this.currentFormElementForContextMenu.alias
+          );
+          value
+            ? mainGroupFormControl?.enable()
+            : mainGroupFormControl?.disable();
+          this.toastrService.success('Form element state is changed!');
+          this.tileOps.saveFormTemplate();
+        },
+      },
     },
   ];
 
-  constructor() {
+  formContextMenuFormGroup!: FormGroup;
+
+  constructor(
+    protected fb: FormBuilder,
+    protected mainFormGroup: FormGroup,
+    protected tileOps: TileOperations<T>,
+    protected toastrService: ToastrService
+  ) {
     super();
+    this.formContextMenuFormGroup = this.fb.group({});
+    this.allActions
+      .map((a) => a.appEntity)
+      .forEach(
+        (c) =>
+          !!c &&
+          tileOps.addControlsToFormGroup(
+            c.alias,
+            c.mainControl,
+            this.formContextMenuFormGroup
+          )
+      );
   }
 }
